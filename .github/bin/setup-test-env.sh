4c4df990bde6231be3ab76626dd336b296f9ef7b
Chris Reynolds <chris@jazzsequence.com>
2025-03-04 15:18:26 -0700

    shellcheck

--- .github/bin/setup-test-env.sh
+++ .github/bin/setup-test-env.sh
@@ -91,8 +91,8 @@ set_multidev() {
 	fi
 
 	# Setup the Drupal site.
-	terminus drush "${site_id}"."pr-${pr_num}" -- si -y
-	terminus drush "${site_id}"."pr-${pr_num}" -- cr
+	terminus drush "${site_id}.pr-${pr_num}" -- si -y
+	terminus drush "${site_id}.pr-${pr_num}" -- cr
 }
 
 update_pantheon_php_version() {
@@ -117,7 +117,7 @@ update_pantheon_php_version() {
 
 copy_bad_module() {
 	echo -e "${YELLOW}Checking if TLS testing module exists...${RESET}"
-	if ! terminus drush "${site_id}"."pr-${pr_num}" -- pm:list --type=module --field=name | grep -q tls_checker_test; then
+	if ! terminus drush "${site_id}.pr-${pr_num}" -- pm:list --type=module --field=name | grep -q tls_checker_test; then
 		cp -r "${workspace}"/.github/fixtures/tls_checker_test ~/pantheon-local-copies/"${site_id}"/web/modules/custom
 	else
 		echo "Test module already installed"
@@ -134,7 +134,7 @@ copy_pr_updates() {
 	git add -A
 	git commit -m "Update to latest commit: ${commit_msg}" || true
 	git push origin "pr-${pr_num}" || true
-	terminus workflow:wait "${site_id}"."pr-${pr_num}"
+	terminus workflow:wait "${site_id}.pr-${pr_num}"
 }
 
 # Run the steps
